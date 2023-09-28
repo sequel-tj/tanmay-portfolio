7f2f83960ee8b736645d23424f0c5f714cea5284
= <=>
2023-09-28 06:24:19 +0530

    skills sections improved

--- js/skills.js
+++ js/skills.js
@@ -1,7 +1,3 @@
-const progressBars = document.getElementsByClassName('progressBar');
-
-function loadProgressBar() {
-    [...progressBars].forEach((ele) => {
-        ele.style.animation = "loading 3s ease-in-out forwards";
-    });
-}
+new Draggable.Draggable(document.querySelector('.imgBx'), {
+    draggable: 'img'
+});
