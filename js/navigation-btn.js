e5f6c848d5b44559ab956f5be2ee57ab8525083e
= <=>
2024-06-02 01:38:05 +0530

    navbar removed

---
+++ js/navigation-btn.js
@@ -0,0 +1,27 @@
+var sections = ["home", "about", "myEducation", "skills", "projects", "contacts"];
+var currentSectionIndex = 0;
+
+function navigateToNextSection() {
+    currentSectionIndex++;
+
+    if (currentSectionIndex >= sections.length) {
+        currentSectionIndex = 0;
+    }
+
+    var nextSection = document.getElementById(sections[currentSectionIndex]);
+    nextSection.scrollIntoView({ behavior: "smooth" });
+}
+
+function navigateToPreviousSection() {
+    currentSectionIndex--;
+
+    if (currentSectionIndex < 0) {
+        currentSectionIndex = sections.length - 1;
+    }
+
+    var previousSection = document.getElementById(sections[currentSectionIndex]);
+    previousSection.scrollIntoView({ behavior: "smooth" });
+}
+
+document.getElementById("navigationUpButton").addEventListener("click", navigateToPreviousSection);
+document.getElementById("navigationDownButton").addEventListener("click", navigateToNextSection);
